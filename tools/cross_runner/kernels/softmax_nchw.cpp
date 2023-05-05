794987a6a1ef7edf3a74fdd0027c0b465cbd5efe
Marcinkowski, Szymon <szymon.marcinkowski@intel.com>
2023-05-05 14:23:22 +0200

    small improvments to kernel

--- tools/cross_runner/kernels/softmax_nchw.cpp
+++ tools/cross_runner/kernels/softmax_nchw.cpp
@@ -2,6 +2,7 @@
 #include <cm/cmtl.h>
 
 #define DT half
+#define DT_ACCU float 
 
 #define ITEMNUM_PER_HW_PACKED ((ITEMNUM_PER_HW * sizeof(DT))/sizeof(uint32_t))
 #define MATH_E 2.7182818f
@@ -18,21 +19,31 @@ extern "C" _GENX_MAIN_ void softmax_nchw(
 	
 	vector<uint32_t, ITEMNUM_PER_HW_PACKED> in_data_packed = cm_load<uint32_t, ITEMNUM_PER_HW_PACKED, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input, in_out_offset);
 	vector_ref<DT, ITEMNUM_PER_HW> my_data = in_data_packed.format<DT>();
+	
+	// do the local (hw) reduce 
 	my_data = cm_pow(MATH_E, my_data);
-	vector<float, 1> my_sum = cm_sum<float>(my_data);
+	vector<DT_ACCU, 1> my_sum = cm_sum<DT_ACCU>(my_data);
 	
 #if LWS_SIZE_X > 1
-	cm_slm_init(LWS_SIZE_X * sizeof(float));
-	//vector<uint32_t, 1> offset(global_x);
-	cm_store_slm<float, 1>(global_x * sizeof(float), my_sum);
+	// store to slm to share partially reduced sums
+	cm_slm_init(LWS_SIZE_X * sizeof(DT_ACCU));
+	cm_store_slm<DT_ACCU, 1>(global_x * sizeof(DT_ACCU), my_sum);
 	cm_slm_fence(CM_GLOBAL_COHERENT_FENCE);
     cm_barrier();
+	
+	// read from slm and further reduce
+	vector<DT_ACCU, LWS_SIZE_X> all_threads_sums = cm_load_slm<DT_ACCU, LWS_SIZE_X>(0);
+	my_sum = cm_sum<DT_ACCU>(all_threads_sums);
 #endif
 	
-	vector<float, LWS_SIZE_X> all_threads_sums = cm_load_slm<float, LWS_SIZE_X>(0);
-	my_sum = cm_sum<float>(all_threads_sums);
-	my_data = my_data / my_sum[0];
+	// do the division in full preicison
+	vector<DT_ACCU, ITEMNUM_PER_HW> my_data_accu = my_data;
+	my_data_accu = my_data_accu / my_sum[0];
 	
-	vector<uint32_t, ITEMNUM_PER_HW_PACKED> out_data_packed = in_data_packed;
+	// cast back to inout data type
+	my_data = my_data_accu;
+	
+	// store results
+	vector_ref<uint32_t, ITEMNUM_PER_HW_PACKED> out_data_packed = in_data_packed;
     cm_store<uint32_t, ITEMNUM_PER_HW_PACKED, DataSize::Default, CacheHint::WriteBack, CacheHint::WriteBack>(surface_output, in_out_offset, out_data_packed);
 }
