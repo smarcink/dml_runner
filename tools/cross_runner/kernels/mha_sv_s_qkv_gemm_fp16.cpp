734dcbad36a6bb9429d1aedabee54bc8cd4a05fd
Marcinkowski, Szymon <szymon.marcinkowski@intel.com>
2023-05-17 10:59:49 +0200

    a bit different approach to the kernel

--- tools/cross_runner/kernels/mha_sv_s_qkv_gemm_fp16.cpp
+++ tools/cross_runner/kernels/mha_sv_s_qkv_gemm_fp16.cpp
@@ -55,21 +55,24 @@ extern "C" _GENX_MAIN_ void mha_sv_s_qka_gemm(
 	
 	matrix<DT_ACCU, TILE_M, TILE_N> accu(0.0f);
 	
-	vector<DT, TILE_K * 2> input_b;
-	vector_ref<uint32_t, TILE_K> input_b_packed = input_b.format<uint32_t>();
+	matrix<DT, TILE_K, TILE_N> input_b;
+	matrix_ref<uint32_t, TILE_K, TILE_N/2> input_b_packed = input_b.format<uint32_t, TILE_K, TILE_N/2>();
 	
-	const uint32_t load_simd_size = 16;
-	const uint32_t packed_eles = sizeof(uint32_t) / sizeof(DT);
-	const uint32_t load_eles = load_simd_size * packed_eles;
-	const uint32_t ks = 2;
-	const uint32_t ksp = ks/packed_eles;
-	vector<uint32_t, 16> input_b_offsets(init_linear_offsets);
-    input_b_offsets += get_input_b_base_offset(thread_id_0, thread_id_1, thread_id_2, batch_thread_offset, head_thread_offset, k_slice_thread_offset);
+    const uint32_t input_b_base_offset= get_input_b_base_offset(thread_id_0, thread_id_1, thread_id_2, batch_thread_offset, head_thread_offset, k_slice_thread_offset);
 	#pragma unroll
-	for(int j = 0; j < TILE_K / load_simd_size; j++)
+	for(int k = 0; k < TILE_K; k++)
 	{
-		input_b_packed.select<load_simd_size * ksp, 1>(j * load_simd_size * ksp) = cm_load<uint32_t, details::lsc_vector_size<ksp>(), DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_qkv, input_b_offsets);  
-		input_b_offsets += load_simd_size * INPUT_B_OFFSET;
+        const uint32_t input_b_offset = input_b_base_offset + k * SIZE_NUM_HEADS * SIZE_STACKED_TENSORS * SIZE_HEAD_SIZE * sizeof(DT);
+		vector_ref<uint32_t, TILE_N/2> packed_row = input_b_packed.row(k);
+#if TILE_K == 40
+        packed_row.select<16, 1>() = cm_load<uint32_t, 16, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_qkv, input_b_offset);
+        packed_row.select<4, 1>(16) = cm_load<uint32_t, 4, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_qkv, input_b_offset + 16 * sizeof(uint32_t));
+#elif TILE_K == 80		
+        packed_row.select<32, 1>() = cm_load<uint32_t, 32, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_qkv, input_b_offset);
+        packed_row.select<8, 1>(32) = cm_load<uint32_t, 8, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_qkv, input_b_offset + 32 * sizeof(uint32_t));
+#else
+        packed_row = cm_load<uint32_t, TILE_N/2, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_qkv, input_b_offset);
+#endif
 	}
 	
 	
@@ -86,12 +89,10 @@ extern "C" _GENX_MAIN_ void mha_sv_s_qka_gemm(
         vector<uint32_t, TILE_K/2> input_a_packed = cm_load<uint32_t, input_a_load_size, DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_s, input_a_offset);
 		vector_ref<DT, TILE_K> input_a = input_a_packed.format<DT>();
 		#pragma unroll
-		for(int n = 0; n < TILE_N; n++)
+		for(int k = 0; k < TILE_K; k++)
 		{
-			vector<DT, TILE_K> temp = input_a * input_b.select<TILE_K, 2>(n);
-			accu[m][n] = cm_sum<DT>(temp);
+			accu.row(m) += input_b.row(k) * input_a.select<1, 1>(k).replicate<TILE_N>();
 		}
-		
     }
 	
 #if SLICE_K > 1
