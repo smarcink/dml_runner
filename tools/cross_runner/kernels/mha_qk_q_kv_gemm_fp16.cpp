f66fe488843cbd4ff3cbb8e01794c5238c0595b8
Marcinkowski, Szymon <szymon.marcinkowski@intel.com>
2023-06-13 13:14:47 +0200

    kernel seems to work, but needs more testing

--- tools/cross_runner/kernels/mha_qk_q_kv_gemm_fp16.cpp
+++ tools/cross_runner/kernels/mha_qk_q_kv_gemm_fp16.cpp
@@ -9,6 +9,18 @@
 #define DT_ACCU DT
 #endif
 
+#if SIZE_N != TILE_N
+#error Kernel currently supports only SIZE_N == TILE_N case.
+#endif
+
+#if SIZE_N == 77
+#define NON_ALIGNED_WIDTH 1
+#define ALIGNED_WIDTH 80
+#define ALIGNED_WIDTH_LOAD_SIZE 16
+#else
+#define ALIGNED_WIDTH TILE_N
+#endif
+
 #define INPUT_B_OFFSET ((SIZE_NUM_HEADS * SIZE_STACKED_TENSORS * SIZE_HEAD_SIZE)* sizeof(DT))
 static const int32_t init_linear_offsets[] = {  0  * INPUT_B_OFFSET,
 											    1  * INPUT_B_OFFSET, 
@@ -28,6 +40,24 @@ static const int32_t init_linear_offsets[] = {  0  * INPUT_B_OFFSET,
 											    15 * INPUT_B_OFFSET,
 											  };
 
+static const int32_t init_out_linear_offsets[] = {  0  * sizeof(DT),
+											        1  * sizeof(DT), 
+											        2  * sizeof(DT),
+											        3  * sizeof(DT),
+											        4  * sizeof(DT),
+											        5  * sizeof(DT),
+											        6  * sizeof(DT),
+											        7  * sizeof(DT),
+												    8  * sizeof(DT), 
+											        9  * sizeof(DT),
+											        10 * sizeof(DT),
+											        11 * sizeof(DT),
+											        12 * sizeof(DT),
+											        13 * sizeof(DT),
+											        14 * sizeof(DT),
+											        15 * sizeof(DT),
+											  };
+
 _GENX_ inline uint32_t get_input_b_base_offset(uint32_t thread_id_0, uint32_t thread_id_1, uint32_t thread_id_2, uint32_t batch_thread_offset, uint32_t head_thread_offset)
 {    
 	return ( batch_thread_offset * SIZE_SEQ_LEN * SIZE_NUM_HEADS * SIZE_STACKED_TENSORS * SIZE_HEAD_SIZE
@@ -36,7 +66,7 @@ _GENX_ inline uint32_t get_input_b_base_offset(uint32_t thread_id_0, uint32_t th
 }
 
 extern "C" _GENX_MAIN_ void mha_qk_q_kv_gemm_fp16(
-	SurfaceIndex surface_input_q [[type("buffer_t")]],   // Q or S input
+	SurfaceIndex surface_input_q [[type("buffer_t")]],
 	SurfaceIndex surface_input_kv [[type("buffer_t")]],
 	SurfaceIndex surface_output [[type("buffer_t")]]
 )
@@ -54,6 +84,10 @@ extern "C" _GENX_MAIN_ void mha_qk_q_kv_gemm_fp16(
 			+ thread_id_0 * TILE_M * SIZE_C * SIZE_K) * sizeof(DT);
 	const uint32_t input_a_load_size = (TILE_K * sizeof(DT)) / sizeof(uint32_t);
 	
+#if NON_ALIGNED_WIDTH
+	vector<ushort, ALIGNED_WIDTH> predicate(1);
+	predicate.select<ALIGNED_WIDTH-SIZE_N, 1>(SIZE_N) = 0;
+#endif
 	
     matrix<DT, TILE_M, TILE_K> input;   
     matrix_ref<uint32_t, TILE_M, TILE_K/2> input_packed = input.format<uint32_t, TILE_M, TILE_K/2>();
@@ -77,9 +111,8 @@ extern "C" _GENX_MAIN_ void mha_qk_q_kv_gemm_fp16(
 	}
 	
 	
-    matrix<DT_ACCU, TILE_M, TILE_N> accu(0.0f);
+    matrix<DT_ACCU, TILE_M, ALIGNED_WIDTH> accu(0.0f);
 	
-	const uint32_t input_b_load_size = (TILE_N * sizeof(DT)) / sizeof(uint32_t);
 	const uint32_t input_b_base_offset = get_input_b_base_offset(thread_id_0, thread_id_1, thread_id_2, batch_thread_offset, head_thread_offset);
 	uint32_t input_b_offset = input_b_base_offset;
 	
@@ -95,31 +128,31 @@ extern "C" _GENX_MAIN_ void mha_qk_q_kv_gemm_fp16(
     for(uint32_t k_chunk = 0; k_chunk < K_PER_THREAD/ks; k_chunk++)
     {
 		vector<uint32_t, load_simd_size> offsets(base_b_offsets);
-		vector<uint32_t, TILE_N * ksp> input_b_packed;
-		vector_ref<DT, TILE_N * ks> input_b_line = input_b_packed.format<DT>();
+		vector<uint32_t, ALIGNED_WIDTH * ksp> input_b_packed;
+		vector_ref<DT, ALIGNED_WIDTH * ks> input_b_line = input_b_packed.format<DT>();
 		#pragma unroll
-		for(int j = 0; j < TILE_N / load_simd_size; j++)
+		for(int j = 0; j < ALIGNED_WIDTH / load_simd_size; j++)
 		{
-			input_b_packed.select<load_simd_size * ksp, 1>(j * load_simd_size * ksp) = cm_load<uint32_t, details::lsc_vector_size<ksp>(), DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_kv, offsets);  
+			input_b_packed.select<load_simd_size * ksp, 1>(j * load_simd_size * ksp) = cm_load<uint32_t, details::lsc_vector_size<ksp>(), DataSize::Default, CacheHint::Cached, CacheHint::Cached>(surface_input_kv, offsets, predicate.select<ALIGNED_WIDTH_LOAD_SIZE, 1>(j * ALIGNED_WIDTH_LOAD_SIZE));  
 			offsets += load_simd_size * INPUT_B_OFFSET;
 		}
 
 		#pragma unroll
 		for(int k = 0; k < ksp; k++)
 		{
-			vector<DT, TILE_N * packed_eles> input_b_ksp_chunk;
+			vector<DT, ALIGNED_WIDTH * packed_eles> input_b_ksp_chunk;
 			#pragma unroll
-			for(int j = 0; j < TILE_N / load_simd_size; j++)
+			for(int j = 0; j < ALIGNED_WIDTH / load_simd_size; j++)
 			{
 				input_b_ksp_chunk.select<load_eles, 1>(j * load_eles) = input_b_line.select<load_eles, 1>(k * load_eles + j * ksp * load_eles);
 			}
 			for(int i = 0; i < packed_eles; i++)
 			{
-				vector<DT, TILE_N> input_b = input_b_ksp_chunk.select<TILE_N, packed_eles>(i);
+				vector<DT, ALIGNED_WIDTH> input_b = input_b_ksp_chunk.select<ALIGNED_WIDTH, packed_eles>(i);
 				#pragma unroll
 				for(uint32_t j = 0; j < TILE_M; j++)
 				{
-					accu.select<1, 1, TILE_N, 1>(j, 0) += input_b * input.select<1, 1, 1, 1>(j, k_chunk * ks + k * packed_eles + i).replicate<TILE_N>();
+					accu.select<1, 1, ALIGNED_WIDTH, 1>(j, 0) += input_b * input.select<1, 1, 1, 1>(j, k_chunk * ks + k * packed_eles + i).replicate<ALIGNED_WIDTH>();
 				}
 			}
 			
@@ -127,21 +160,42 @@ extern "C" _GENX_MAIN_ void mha_qk_q_kv_gemm_fp16(
 		base_b_offsets += ksp * sizeof(uint32_t);
     }
 	
-	const uint32_t output_store_size = (TILE_N * sizeof(DT)) / sizeof(uint32_t);
-    uint32_t output_offset = 
-				cm_group_id(2) * SIZE_M * SIZE_N * sizeof(DT)
-				+ thread_id_0 * TILE_M * SIZE_N * sizeof(DT)
-				+ (thread_id_1 * TILE_N * sizeof(DT));
+	const uint32_t output_store_size = (ALIGNED_WIDTH * sizeof(DT)) / sizeof(uint32_t);
+    uint32_t output_offset = (thread_id_2 * SIZE_M * SIZE_N  + thread_id_0 * TILE_M * SIZE_N) * sizeof(DT);
 				
-	//printf("[%d, %d]: %d\n", batch_channels_thread_offset, output_offset, batch_channels_thread_offset * SIZE_M * SIZE_N * sizeof(DT));
-				
-	matrix<DT, TILE_M, TILE_N> accu_out = accu;  // if DT_ACCU == DT then compiler removes this line
+
+	matrix<DT, TILE_M, ALIGNED_WIDTH> accu_out = accu;  // if DT_ACCU == DT then compiler removes this line
 	accu_out *= DT(SCALE);
+	
+		// store results
+#if NON_ALIGNED_WIDTH
+	vector<uint32_t, ALIGNED_WIDTH_LOAD_SIZE> store_base_offsets(init_out_linear_offsets);
+	store_base_offsets += output_offset;
+	
+	#pragma unroll
+	for(int m = 0; m < TILE_M; m++)
+	{
+		vector<uint32_t, ALIGNED_WIDTH_LOAD_SIZE> store_offsets(store_base_offsets);
+		store_offsets += m * SIZE_N * sizeof(DT);
+		
+		vector_ref<DT, ALIGNED_WIDTH> accu_row = accu_out.row(m);
+		
+		#pragma unroll
+		for(int i = 0; i < (ALIGNED_WIDTH / ALIGNED_WIDTH_LOAD_SIZE); i++)
+		{
+			cm_store<DT, VectorSize::N1, DataSize::Default, CacheHint::WriteBack, CacheHint::WriteBack>(surface_output, store_offsets, accu_row.select<ALIGNED_WIDTH_LOAD_SIZE, 1>(i * ALIGNED_WIDTH_LOAD_SIZE), predicate.select<ALIGNED_WIDTH_LOAD_SIZE, 1>(i * ALIGNED_WIDTH_LOAD_SIZE));
+			store_offsets += ALIGNED_WIDTH_LOAD_SIZE * sizeof(DT);
+		}
+	}
+	
+
+#else
 	#pragma unroll
     for(uint32_t i = 0; i < TILE_M; i++)
     {
-        vector_ref<uint32_t, output_store_size> accu_0_packed = accu_out.select<1, 1, TILE_N, 1>(i, 0).format<uint32_t>();
+        vector_ref<uint32_t, output_store_size> accu_0_packed = accu_out.select<1, 1, ALIGNED_WIDTH, 1>(i, 0).format<uint32_t>();
         cm_store<uint32_t, output_store_size, DataSize::Default, CacheHint::WriteBack, CacheHint::WriteBack>(surface_output, output_offset, accu_0_packed);
         output_offset += SIZE_N * sizeof(DT);
     }
+#endif
 }
