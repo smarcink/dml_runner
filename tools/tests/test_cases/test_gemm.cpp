c27ad1c63bf3ae30b28c26f84afae1000f4c5257
Marcinkowski, Szymon <szymon.marcinkowski@intel.com>
2023-11-28 17:38:37 +0100

    gtest as submodule

--- tools/tests/test_cases/test_gemm.cpp
+++ tools/tests/test_cases/test_gemm.cpp
@@ -1,1 +1,41 @@
 
+#include <gtest/gtest.h>
+
+struct TestShapes
+{
+    std::size_t B;
+    std::size_t M;
+    std::size_t K;
+    std::size_t N;
+};
+
+
+// The fixture for testing class Foo.
+class DnnlPluginNext_GEMM : public testing::TestWithParam<int>
+{
+protected:
+    DnnlPluginNext_GEMM() {
+        // You can do set-up work for each test here.
+    }
+
+    ~DnnlPluginNext_GEMM() override {
+        // You can do clean-up work that doesn't throw exceptions here.
+    }
+
+    bool run()
+    {
+        return true;
+    }
+
+private:
+
+};
+
+
+TEST_P(DnnlPluginNext_GEMM, TestParametrized)
+{
+    EXPECT_TRUE(run());
+}
+
+//INSTANTIATE_TEST_SUITE_P(TestsFP32, DnnlPluginNext_GEMM, testing::Range(0, 10),
+//    testing::PrintToStringParamName());
