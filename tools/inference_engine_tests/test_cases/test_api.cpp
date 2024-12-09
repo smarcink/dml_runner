c90a2a97b6c272d66e7622543957ab6c48aba7f1
y <szymon.marcinkowski@intel.com>
2024-12-09 16:16:05 +0100

    TEST(ApiTest, get_outputs_multiple_outs)

--- tools/inference_engine_tests/test_cases/test_api.cpp
+++ tools/inference_engine_tests/test_cases/test_api.cpp
@@ -32,99 +32,42 @@ TEST(ApiTest, get_outputs_single_out)
 
     const auto& input = input_mappings[input_node];
     const auto& out_mapping = output_mappings[out_node];
-    ASSERT_TRUE(output_mappings.find(out_node) != std::end(output_mappings));
+    ASSERT_NE(output_mappings.find(out_node), std::end(output_mappings));
     ASSERT_EQ(out_mapping.data_type, input.data_type);
     ASSERT_EQ(out_mapping.dims.size(), input.dims.size());
     for (auto i = 0; i < out_mapping.dims.size(); i++)
     {
         ASSERT_EQ(out_mapping.dims[i], input.dims[i]);
     }
-    //auto device = reinterpret_cast<inference_engine_device_t>(G_DX12_ENGINE.d3d12_device.Get());
-    //auto stream = reinterpret_cast<inference_engine_stream_t>(G_DX12_ENGINE.command_list.Get());
-    //auto ctx = inferenceEngineCreateContext(device, fill_with_dx12_callbacks());
-    //EXPECT_TRUE(nullptr != ctx);
-
-    //auto model_desc = inferenceEngineCreateModelDescriptor();
-
-    //inference_engine_port_desc_t input_desc{};
-    //input_desc.data_type = inference_engine_data_type_t::INFERENCE_ENGINE_DATA_TYPE_FP16;
-    //auto input_node = inferenceEngineModelDescriptorAddPort(model_desc, input_desc);
-
-    //inference_engine_activation_desc_t activation_desc{};
-    //activation_desc.type = inference_engine_activation_type_t::INFERENCE_ENGINE_ACTIVATION_TYPE_RELU;
-    //activation_desc.input = input_node;
-    //auto out_node = inferenceEngineModelDescriptorAddActivation(model_desc, activation_desc);
-
-    //inference_engine_tensor_mapping_t input{};
-    //input.id = input_node;
-    //input.tensor.data_type = input_desc.data_type;
-    //set_array(input.tensor.dims, 1, 2, 4, 4);
-    //auto model = inferenceEngineCompileModelDescriptor(ctx, stream, model_desc, &input, 1);
-    //ASSERT_NE(model, nullptr);
-
-    //std::size_t outputs_counts = 0;
-    //ASSERT_EQ(inferenceEngineModelGetOutputs(model, nullptr, &outputs_counts), true);
-    //ASSERT_EQ(outputs_counts, 1);
-    //std::vector<inference_engine_tensor_mapping_t> output_mappings(outputs_counts);
-    //ASSERT_EQ(inferenceEngineModelGetOutputs(model, output_mappings.data(), &outputs_counts), true);
-    //ASSERT_EQ(output_mappings[0].id, out_node);
-    //ASSERT_EQ(output_mappings[0].tensor.data_type, input.tensor.data_type);
-    //for (auto i = 0; i < 4; i++)
-    //{
-    //    ASSERT_EQ(output_mappings[0].tensor.dims[i], input.tensor.dims[i]);
-    //}
-    //inferenceEngineDestroyModel(model);
-    //inferenceEngineDestroyModelDescriptor(model_desc);
-    //inferenceEngineDestroyContext(ctx);
 }
 
 TEST(ApiTest, get_outputs_multiple_outs)
 {   
-    auto device = reinterpret_cast<inference_engine_device_t>(G_DX12_ENGINE.d3d12_device.Get());
-    auto stream = reinterpret_cast<inference_engine_stream_t>(G_DX12_ENGINE.command_list.Get());
-    auto ctx = inferenceEngineCreateContext(device, fill_with_dx12_callbacks());
-    EXPECT_TRUE(nullptr != ctx);
+    DeviceDX12 device(G_DX12_ENGINE.d3d12_device);
+    StreamDX12 stream(G_DX12_ENGINE.command_list);
+    ContextDX12 ctx(device);
 
-    auto model_desc = inferenceEngineCreateModelDescriptor();
+    inference_engine::ModelDescriptor md{};
+    auto input_node = md.add_port(inference_engine_port_desc_t{ INFERENCE_ENGINE_DATA_TYPE_FP16 });
 
-    inference_engine_port_desc_t input_desc{};
-    input_desc.data_type = inference_engine_data_type_t::INFERENCE_ENGINE_DATA_TYPE_FP16;
-    auto input_node = inferenceEngineModelDescriptorAddPort(model_desc, input_desc);
-
-    std::vector<inference_engine_node_id_t> output_nodes{};
-    inference_engine_activation_desc_t activation_desc{};
-    activation_desc.type = inference_engine_activation_type_t::INFERENCE_ENGINE_ACTIVATION_TYPE_RELU;
-    activation_desc.input = input_node;
+    std::vector<inference_engine::NodeID> output_nodes{};
     for (auto i = 0; i < 6; i++)
     {
-        auto node = inferenceEngineModelDescriptorAddActivation(model_desc, activation_desc);
-        ASSERT_NE(node, INFERENCE_ENGINE_INVALID_NODE_ID);
+        auto node = md.add_activation(inference_engine_activation_desc_t{ input_node, INFERENCE_ENGINE_ACTIVATION_TYPE_RELU });
         output_nodes.push_back(node);
     }
-    
-    inference_engine_tensor_mapping_t input{};
-    input.id = input_node;
-    input.tensor.data_type = input_desc.data_type;
-    set_array(input.tensor.dims, 1, 1, 1, 64);
-    auto model = inferenceEngineCompileModelDescriptor(ctx, stream, model_desc, &input, 1);
-    ASSERT_NE(model, nullptr);
 
-    std::size_t outputs_counts = 0;
-    ASSERT_EQ(inferenceEngineModelGetOutputs(model, nullptr, &outputs_counts), true);
-    ASSERT_EQ(outputs_counts, output_nodes.size());
-    std::vector<inference_engine_tensor_mapping_t> output_mappings(outputs_counts);
-    ASSERT_EQ(inferenceEngineModelGetOutputs(model, output_mappings.data(), &outputs_counts), true);
+    inference_engine::TensorMapping input_mappings{};
+    input_mappings[input_node] = inference_engine::Tensor(INFERENCE_ENGINE_DATA_TYPE_FP16, { 1, 1, 1, 64 });
+    auto model = inference_engine::Model(ctx, stream, md, input_mappings);
+
+    auto output_mappings = model.get_outputs();
+    ASSERT_EQ(output_mappings.size(), output_nodes.size());
+
     for (auto& on : output_nodes)
     {
-        auto it = std::find_if(std::begin(output_mappings), std::end(output_mappings), [&on](const auto& om)
-            {
-                return on == om.id;
-            });
-        ASSERT_NE(it, std::end(output_mappings));
+        ASSERT_NE(output_mappings.find(on), std::end(output_mappings));
     }
-    inferenceEngineDestroyModel(model);
-    inferenceEngineDestroyModelDescriptor(model_desc);
-    inferenceEngineDestroyContext(ctx);
 }
 
 TEST(ApiTest, get_outputs_null)
